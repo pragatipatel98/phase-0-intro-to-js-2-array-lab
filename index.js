900f89f5125ca34fd302de8b3bd23b7cd113804f
Pragati Patel <pragatinpatel@gmail.com>
2023-03-09 17:54:51 -0500

    adds solution code to index.js file - lab complete

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+ cats.push("Ralph")
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+
+};
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop("Garfield");
+};
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+    cats;
+}
+
+function removeLastCat(name) {
+    const copyCats = cats.slice()
+    return copyCats.slice(0, copyCats.length - 1 );
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+
+function removeFirstCat() {
+    return cats.slice(cats.length - 2)
+}
+
+function prependCat(name) {
+    const cats = ["Arnold", "Milo", "Otis", "Garfield"]
+    return [...cats];
+}
